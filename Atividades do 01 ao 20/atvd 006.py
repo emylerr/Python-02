4dd6179186ddc55762209e720fcca30d9f15696f
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-09 13:45:37 -0300

    Python

--- Atividades do 01 ao 20/atvd 006.py
+++ Atividades do 01 ao 20/atvd 006.py
@@ -2,4 +2,9 @@ import pygame
 pygame.init()
 pygame.mixer.music.load('atvd6.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+
+print("Música iniciada! Pressione ENTER para fechar o programa...")
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+pygame.quit()
