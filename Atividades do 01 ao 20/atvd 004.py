6ac77dcb0f482826a65a00254780b5ceecc6fb5f
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-09 13:09:37 -0300

    Python

--- Atividades do 01 ao 20/atvd 004.py
+++ Atividades do 01 ao 20/atvd 004.py
@@ -1,5 +1,8 @@
 import random
-nomes = ['Nathan', 'Gabriel', 'Rafael', 'Ismael']
-print(*nomes, sep=', ')
-sort = random.choice(nomes)
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3= str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+sort = random.choice(lista)
 print(f'O nome sorteado foi: {sort}!')
