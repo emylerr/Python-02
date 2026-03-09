520cd2e1055eb64a52680896f4719401bf2b3d3f
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-09 13:02:32 -0300

    Python

--- Atividades do 01 ao 20/atvd 003.py
+++ Atividades do 01 ao 20/atvd 003.py
@@ -1,10 +1,7 @@
 import math
 ang = float(input('Digite um valor de ângulo: '))
 rad = math.radians(ang)
-seno = math.sin(rad)
-coss = math.cos(rad)
-tang = math.tan(rad)
 print(f'O ângulo é de: {ang}')
-print(f'O seno é de: {seno:.2f}')
-print(f'O cosseno é de: {coss:.2f}')
-print(f'A tangente é de: {tang:.2f}')
+print(f'O seno é de: {math.sin(rad):.2f}')
+print(f'O cosseno é de: {math.cos(rad):.2f}')
+print(f'A tangente é de: {math.tan(rad):.2f}')
