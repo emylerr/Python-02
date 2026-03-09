323054185322e77171914f7e52f307370d358d28
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-09 12:54:25 -0300

    Python

--- Atividades do 01 ao 20/atvd 001.py
+++ Atividades do 01 ao 20/atvd 001.py
@@ -1,4 +1,3 @@
 import math
 r = float(input('Digite um número real: '))
-int = math.trunc(r)
-print(f'O número {r} tem a parte inteira {int}!')
+print(f'O número {r} tem a parte inteira {math.trunc(r)}!')
--- Atividades do 01 ao 20/atvd 002.py
+++ Atividades do 01 ao 20/atvd 002.py
@@ -1,5 +1,4 @@
 import math
 co = float(input('Digite o valor do cateto oposto: '))
 ca = float(input('Digite o valor do cateto adjacente: '))
-hip = math.hypot(co, ca)
-print(f'A hipotenusa é {hip:.2f}')
+print(f'A hipotenusa é {math.hypot(co, ca):.2f}')
